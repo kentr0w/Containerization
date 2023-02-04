27cd6a1588d2656cfb4355e32daf44079c07b013
DockerizedJ <>
2023-02-04 19:36:10 +0000

    reload after form submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 					<input name="email" type="text"/><br/>
 					<label for="description">Description:</label><br/>
 					<input name="description" type="text"/><br/>
-					<input type="submit" value="register"/>
+					<input type="submit" value="register" onclick="javascript:setTimeout(() => location.reload(), 300);" />
 				</form>
 				<hr/>
 				<h1>List Of Previous Business Cards:</h1>
