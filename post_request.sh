638f7a9e9a3bea126fa41c3f060ce037e3ac18aa
zelyior <noe.zhangnl@gmail.com>
2023-02-05 18:51:58 +0000

    final version

--- post_request.sh
+++
@@ -1,2 +0,0 @@
-curl -i -H 'Content-type: application/json' -X POST -d \
-'{"firstname":"jean", "lastname":"claude","job_title":"ingeneer","email":"test@gmail.com","description":"a small desc."}' http://localhost:5000/users/
